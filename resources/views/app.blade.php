fa97d9cd89afb802ed8c16317d20014e99fc58f3
Christan Jake C. Gatchalian <christan.jake2025@yahoo.com>
2023-10-08 01:06:00 +0800

    Changed laravel to project name. | Added defer to fix the error regarding DOM on load.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,17 +4,17 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Human Resource Information System</title>
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
-        <script src="{{ asset('ts/app.js') }}"></script>
+        <script src="{{ asset('ts/app.js') }}" defer></script>
     </head>
+
     <body class="antialiased">
         <div class="container mx-auto" id="root">
-            @yield('content')
         </div>
     </body>
 </html>

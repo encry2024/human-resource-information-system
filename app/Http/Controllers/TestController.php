c5337e44533051aed58c815cb9163dde58bc9c1a
Christan Jake Gatchalian <jake@globalmobility.net>
2023-10-07 18:15:36 +0800

    Added return type for index function.

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
     //
 
-    public function index()
+    public function index(): Factory|View|Application
     {
         return view('index');
     }

69f78ff0472818eef1123d32bf7f8b1b0328304b
Christan Jake C. Gatchalian <christan.jake2025@yahoo.com>
2023-10-10 15:51:13 +0800

    Update login route.

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/login', [LoginController::class, ['index']]);
+Route::get('/login', [LoginController::class, 'index']);
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index']);

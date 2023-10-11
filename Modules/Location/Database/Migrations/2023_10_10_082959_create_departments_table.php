1d4b91147d706670d5d7eb864b39709f7cd2ed12
Christan Jake C. Gatchalian <christan.jake2025@yahoo.com>
2023-10-11 20:33:48 +0800

    Updated departments table for migration.

--- Modules/Location/Database/Migrations/2023_10_10_082959_create_departments_table.php
+++ Modules/Location/Database/Migrations/2023_10_10_082959_create_departments_table.php
@@ -15,8 +15,10 @@ return new class extends Migration
     {
         Schema::create('departments', function (Blueprint $table) {
             $table->id();
-
+            $table->string('department_code')->index()->nullable();
+            $table->string('name')->index()->unique();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 

689bba3224215b91a3e6a00cb356cc17d0b6898e
Christan Jake C. Gatchalian <christan.jake2025@yahoo.com>
2023-10-11 20:34:05 +0800

    Added unit testing for department module.

---
+++ Modules/Location/Tests/Unit/Department/DepartmentTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Modules\Location\Tests\Unit\Department;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Modules\Location\Entities\Department;
+
+class DepartmentTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}

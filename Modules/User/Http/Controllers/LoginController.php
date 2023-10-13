4ebea2bfee5aa7601dd34765ec80a002c8060f63
Christan Jake C. Gatchalian <christan.jake2025@yahoo.com>
2023-10-13 08:11:38 +0800

    Added test authenticate function for user login.

--- Modules/User/Http/Controllers/LoginController.php
+++ Modules/User/Http/Controllers/LoginController.php
@@ -3,6 +3,7 @@
 namespace Modules\User\Http\Controllers;
 
 use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 
@@ -13,8 +14,13 @@ class LoginController extends Controller
      *
      * @return Renderable
      */
-    public function login(): Renderable
+    public function login() : Renderable
     {
         return view('user::components.login');
+    }
+
+    public function authenticate(Request $request) : JsonResponse
+    {
+        return response()->json($request->all());
     }
 }

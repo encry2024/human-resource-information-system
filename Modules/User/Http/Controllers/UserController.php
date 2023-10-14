6fb50dde9647e74f6094547eff67fc245cb287e0
Christan Jake C. Gatchalian <christan.jake2025@yahoo.com>
2023-10-14 10:21:14 +0800

    Added classes for return type.

--- Modules/User/Http/Controllers/UserController.php
+++ Modules/User/Http/Controllers/UserController.php
@@ -2,17 +2,21 @@
 
 namespace Modules\User\Http\Controllers;
 
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Contracts\View\Factory;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Illuminate\View\View;
 
 class UserController extends Controller
 {
     /**
      * Display a listing of the resource.
-     * @return Renderable
+     *
+     * @return Application|Factory|View
      */
-    public function index()
+    public function index(): Application|Factory|View
     {
         return view('user::index');
     }

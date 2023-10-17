e8439691f760fdc6e4a9f7714de12ec98eb35f4d
Christan Jake C. Gatchalian <christan.jake2025@yahoo.com>
2023-10-17 08:59:06 +0800

    Added Dashboard UI

---
+++ Modules/User/Resources/assets/tsx/components/Dashboard.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Dashboard: React.FC = () => {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+            <div className="bg-white p-8 rounded-lg shadow-lg w-1/3">
+
+                <div className="bg-indigo-500 ml-[-2rem] mb-9">
+                    <h2 className="text-2xl font-semibold mb-4 p-2 text-right text-white">HRIS :: Dashboard</h2>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Dashboard;

fb791334db08925cabc617339e3709f30d1c726f
Christan Jake C. Gatchalian <christan.jake2025@yahoo.com>
2023-10-17 08:58:26 +0800

    Added handle function to include Authorization header for subsequent request.

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,19 +3,32 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Closure;
+use Illuminate\Http\Request;
 
 class Authenticate extends Middleware
 {
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param Request $request
      * @return string|null
      */
-    protected function redirectTo($request)
+    protected function redirectTo($request): ?string
     {
         if (! $request->expectsJson()) {
             return route('login');
         }
+    }
+
+    public function handle($request, Closure $next, ...$guards)
+    {
+        if ($token = $request->cookie('_token')) {
+            $request->headers->set('Authorization', 'Bearer ' . $token);
+        }
+
+        $this->authenticate($request, $guards);
+
+        return $next($request);
     }
 }

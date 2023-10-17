508c3e26fde34eec6f97fb3da0b9ba38d5d7abd2
Christan Jake C. Gatchalian <christan.jake2025@yahoo.com>
2023-10-17 09:05:30 +0800

    Added BrowserRouter for SPA.

--- resources/tsx/Index.tsx
+++ resources/tsx/Index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '../../public/css/app.css';
 import Login from './components/Login';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Dashboard from "../../Modules/User/Resources/assets/tsx/components/Dashboard";
 
 const rootDom: HTMLElement | null = document.getElementById("root");
 
@@ -10,7 +12,12 @@ if (rootDom) {
 
     root.render(
         <React.StrictMode>
-            <Login />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/login" element={<Login />}/>
+                    <Route path="/user/dashboard" element={<Dashboard />}/>
+                </Routes>
+            </BrowserRouter>
         </React.StrictMode>
     );
 }

05ac7f23681616271a4cfaee27a995b805ce223d
Christan Jake C. Gatchalian <christan.jake2025@yahoo.com>
2023-10-30 05:45:06 +0800

    Added welcome label for the user in Sidebar.tsx

--- resources/tsx/components/Sidebar.tsx
+++ resources/tsx/components/Sidebar.tsx
@@ -1,13 +1,9 @@
-import React, {useState} from "react";
-import UserMenu from "./Menu/User/UserMenu";
-import RequestsMenu from "./Menu/Requests/Requests";
+import React from "react";
+import { useUser } from "../../../Modules/User/Resources/assets/tsx/Provider/User/UserProvider";
+
 
 const Sidebar: React.FC = () => {
-    const [subMenuOpen, setSubMenuOpen] = useState(false);
-
-    const toggleSubMenu = () => {
-        setSubMenuOpen(!subMenuOpen);
-    };
+    const { user } = useUser();
 
     return (
         <div className="min-h-screen bg-gray-800 text-white w-64 py-6">
@@ -15,26 +11,41 @@ const Sidebar: React.FC = () => {
 
             <div className="border-t border-gray-500 my-4 w-3/4 mx-auto"></div>
 
+            <div className="text-lg font-bold text-center mb-6">{user && <p>Welcome, {user.username}!</p>}</div>
+
+            <div className="border-t border-gray-500 my-4 w-3/4 mx-auto"></div>
+
             <ul className="mt-6">
                 <li className="mb-2">
-                    <a href="/user/dashboard" className="block text-sm py-2 px-6 hover:bg-gray-700">Dashboard</a>
+                    <a href="/user/dashboard" className="sidebar--link">Dashboard</a>
                 </li>
-                <UserMenu title="Users "/>
+
                 <li className="mb-2">
-                    <a href="#" className="block text-sm py-2 px-6 hover:bg-gray-700">Company Library</a>
+                    <a href="#" className="sidebar--link">Users</a>
                 </li>
+
                 <li className="mb-2">
-                    <a href="#" className="block text-sm py-2 px-6 hover:bg-gray-700">Accounting</a>
+                    <a href="#" className="sidebar--link">Company Library</a>
                 </li>
-                <RequestsMenu title="Requests " />
+
                 <li className="mb-2">
-                    <a href="#" className="block text-sm py-2 px-6 hover:bg-gray-700">Calendar</a>
+                    <a href="#" className="sidebar--link">Accounting</a>
                 </li>
+
                 <li className="mb-2">
-                    <a href="#" className="block text-sm py-2 px-6 hover:bg-gray-700">Trainings</a>
+                    <a href="#" className="sidebar--link">Requests</a>
                 </li>
+
                 <li className="mb-2">
-                    <a href="#" className="block text-sm py-2 px-6 hover:bg-gray-700">Performance</a>
+                    <a href="#" className="sidebar--link">Calendar</a>
+                </li>
+
+                <li className="mb-2">
+                    <a href="#" className="sidebar--link">Trainings</a>
+                </li>
+
+                <li className="mb-2">
+                    <a href="#" className="sidebar--link">Performance</a>
                 </li>
 
                 <div className="border-t border-gray-500 my-4 w-3/4 mx-auto"></div>

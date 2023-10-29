f6c414f415694f1281c51f42471704595ecb1e3e
Christan Jake C. Gatchalian <christan.jake2025@yahoo.com>
2023-10-30 05:47:19 +0800

    Added Helper.tsx, Authentication.tsx, IHRInput.tsx, IUserInput.tsx, User.tsx, UserProvider.tsx.

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,8 +5,15 @@ const $ = require('jquery');
 mix.js('resources/js/app.js', 'public/js')
     .ts([
         'resources/tsx/App.tsx',
-        'resources/tsx/components/Login.tsx',
-        'resources/tsx/Index.tsx'
+        'Modules/User/Resources/assets/tsx/Provider/User/UserProvider.tsx',
+        'Modules/User/Resources/assets/tsx/model/User.tsx',
+        'resources/tsx/Components/Login.tsx',
+        'resources/tsx/Index.tsx',
+        'resources/tsx/Interfaces/IHRInput.tsx',
+        'resources/tsx/Interfaces/IUserInput.tsx',
+        'resources/tsx/Model/User/Authentication.tsx',
+        'resources/tsx/Helper.tsx',
+
     ], 'public/ts/app.js').react()
     .copy('node_modules/jquery/dist/jquery.min.js', 'public/js')
     .options({

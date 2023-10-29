30f135a699c707d7dde36c1adee30d76863319b6
Christan Jake C. Gatchalian <christan.jake2025@yahoo.com>
2023-10-30 04:44:18 +0800

    Fixed incorrect file path.

--- Modules/User/Resources/assets/tsx/components/Dashboard.tsx
+++ Modules/User/Resources/assets/tsx/components/Dashboard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Sidebar from "../../../../../../resources/tsx/components/Sidebar";
+import Sidebar from "../../../../../../resources/tsx/Components/Sidebar";
 
 const Dashboard: React.FC = () => {
     return (

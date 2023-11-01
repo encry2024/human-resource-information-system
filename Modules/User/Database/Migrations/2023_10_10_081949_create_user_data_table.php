3295034232f70d2a594590065961f46deaf9eb43
Christan Jake C. Gatchalian <christan.jake2025@yahoo.com>
2023-11-02 06:23:57 +0800

    Added employee_id column.

--- Modules/User/Database/Migrations/2023_10_10_081949_create_user_data_table.php
+++ Modules/User/Database/Migrations/2023_10_10_081949_create_user_data_table.php
@@ -16,16 +16,17 @@ return new class extends Migration
         Schema::create('user_data', function (Blueprint $table) {
             $table->id();
             $table->bigInteger('user_type_id');
+            $table->string('employee_id')->unique('employee_id')->index('employee_id');
             $table->bigInteger('user_id');
             $table->integer('country_id');
-            $table->string('first_name')->index();
-            $table->string('middle_name')->index();
-            $table->string('last_name')->index();
+            $table->string('first_name')->index('first_name');
+            $table->string('middle_name')->index('middle_name');
+            $table->string('last_name')->index('last_name');
             $table->integer('postal_code');
             $table->text('address');
             $table->string('telephone_number')->nullable();
-            $table->string('mobile_number')->index();
-            $table->string('email')->unique()->index();
+            $table->string('mobile_number')->index('mobile_number');
+            $table->string('email')->unique('email')->index('email');
             $table->date('date_of_birth');
             $table->timestamps();
             $table->softDeletes();
